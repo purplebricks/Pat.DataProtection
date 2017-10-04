ecb5a02a1c9f6ad902af714dafc142e6fa3d3ab3
Abe Rodriguez <Abe.Rodriguez@purplebricks.com>
2017-10-04 17:43:30 +0000

    Merged PR 14721: Updated to netstandard library
    Related work items: #11708

--- PB.ITOps.Messaging.DataProtection/CertificateHelper.cs
+++ PB.ITOps.Messaging.DataProtection/CertificateHelper.cs
@@ -7,31 +7,21 @@ namespace PB.ITOps.Messaging.DataProtection
     {
         public static X509Certificate2 FindCertificateByThumbprint(string findValue)
         {
-            X509Store x509Store1 = new X509Store(StoreName.My, StoreLocation.CurrentUser);
-            try
+            using (X509Store x509Store1 = new X509Store(StoreName.My, StoreLocation.CurrentUser))
             {
                 x509Store1.Open(OpenFlags.ReadOnly);
                 X509Certificate2Collection certificate2Collection = x509Store1.Certificates.Find(X509FindType.FindByThumbprint, (object)findValue, false);
                 if (certificate2Collection.Count == 0)
                 {
-                    X509Store x509Store2 = new X509Store(StoreName.My, StoreLocation.LocalMachine);
-                    try
+                    using (X509Store x509Store2 = new X509Store(StoreName.My, StoreLocation.LocalMachine))
                     {
                         x509Store2.Open(OpenFlags.ReadOnly);
                         certificate2Collection = x509Store2.Certificates.Find(X509FindType.FindByThumbprint, (object)findValue, false);
-                    }
-                    finally
-                    {
-                        x509Store2.Close();
                     }
                     if (certificate2Collection.Count == 0)
                         throw new ArgumentException(string.Format("Could not find certificate with thumbprint starting '{0}'", (object)findValue.Substring(0, 4)));
                 }
                 return certificate2Collection[0];
-            }
-            finally
-            {
-                x509Store1.Close();
             }
         }
     }
